e104418d97c04bacdf4f681da069afc455005970
unknown <heard.jacob.7@gmail.com>
2020-07-10 14:05:05 -0400

    Initial solution

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,58 @@
+from enum import Enum
+
+class Events(Enum):
+    ADD = 1
+    SUBTRACT = 2
+    MULTIPLY = 3
+    DIVIDE = 4
+
+class Event():
+    def __init__(self, value, change, event):
+        self.value = value
+        self.change = change
+        self.event = event
+
 class EventSourcer():
     # Do not change the signature of any functions
 
     def __init__(self):
         self.value = 0
+        self.current_event = -1
+        self.events = []
 
     def add(self, num: int):
-        pass
+        self.current_event += 1
+        self.events.append(Event(self.value, num, Events.ADD))
+        self.value += num
 
     def subtract(self, num: int):
-        pass
+        self.current_event += 1
+        self.events.append(Event(self.value, num, Events.SUBTRACT))
+        self.value -= num
 
     def undo(self):
-        pass
+        # Nothing to undo
+        if self.current_event == -1:
+            return
+        self.value = self.events[self.current_event].value
+        self.current_event -= 1
 
     def redo(self):
-        pass
+        # Nothing to redo
+        if self.current_event == len(self.events)-1:
+            return
+        ev = self.events[self.current_event]
+        if ev.event == Events.ADD:
+            self.value += ev.change
+        if ev.event == Events.SUBTRACT:
+            self.value -= ev.change
+        self.current_event += 1
 
     def bulk_undo(self, steps: int):
-        pass
+        for _ in range(steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        for _ in range(steps):
+            self.redo()
+    
